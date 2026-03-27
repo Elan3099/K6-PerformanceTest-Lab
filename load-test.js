a8341b82166fcc1017681ca39847b33eae17e5e5
graygah <graygahroynando@gmail.com>
2026-03-27 16:23:50 +0700

    Test 2

---
+++ load-test.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 10 }, // Ramp-up ke 10 user
+        { duration: '20s', target: 10 }, // Stay di 10 user
+        { duration: '5s', target: 0 },  // Ramp-down
+    ],
+    thresholds: {
+        // CI/CD akan FAIL jika 95% request lebih dari 250ms
+        http_req_duration: ['p(95)<250'],
+        http_req_failed: ['rate<0.01'], // Gagal jika error > 1%
+    },
+};
+
+export default function () {
+    const res = http.get('http://localhost:3000/api/v1/resource');
+    
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'has correct body': (r) => r.json().status === 'success',
+    });
+
+    sleep(1);
+}
